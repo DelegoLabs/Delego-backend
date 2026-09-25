39eca7145365de70cfbac0efcb504e4208ffcd4c
nissinanlung <nissinanlung@gmail.com>
2026-09-25 20:58:31 +0100

    chore: add feeEstimator index exports (closes #287)

---
+++ apps/backend/wallet/src/feeEstimator/index.ts
@@ -0,0 +1,3 @@
+export { AdaptiveFeeEstimator, createAdaptiveFeeEstimator } from "./adaptiveFeeEstimator.js";
+export type { AdaptiveFeeConfig, CachedFeeEstimate, AdaptiveFeeResult } from "./adaptiveFeeEstimator.js";
+export { DEFAULT_ADAPTIVE_CONFIG } from "./adaptiveFeeEstimator.js";
